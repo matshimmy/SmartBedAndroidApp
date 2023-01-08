1765af6b1dc56739dbabdcc14f059a635b2c2c52
Mathew Szymanowski <m96szy@gmail.com>
2023-01-08 18:17:09 -0500

    fixed back button on main

--- app/src/main/java/com/example/hospitalbedcontrols/MainActivity.kt
+++ app/src/main/java/com/example/hospitalbedcontrols/MainActivity.kt
@@ -1,13 +1,28 @@
 package com.example.hospitalbedcontrols
 
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.example.hospitalbedcontrols.ble.isBLEok
+import com.example.hospitalbedcontrols.model.BluetoothViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: BluetoothViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+        viewModel = ViewModelProvider(this).get(BluetoothViewModel::class.java)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun finish() {
+        super.finish()
+        if (isBLEok(this, viewModel))
+            viewModel.disconnectBleDevice()
     }
 }
--- app/src/main/java/com/example/hospitalbedcontrols/MainFragment.kt
+++ app/src/main/java/com/example/hospitalbedcontrols/MainFragment.kt
@@ -1,17 +1,28 @@
 package com.example.hospitalbedcontrols
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.example.hospitalbedcontrols.adapter.ControlAdapter
 import com.example.hospitalbedcontrols.databinding.FragmentMainBinding
 
+
 class MainFragment : Fragment(R.layout.fragment_main) {
     private lateinit var binding: FragmentMainBinding
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        //Prevents finish() on activity and stopping connection, must logout
+        val callback: OnBackPressedCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                Navigation.findNavController(view).navigateUp()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
+
         binding = FragmentMainBinding.bind(view)
         binding.controlsRecyclerView.adapter = ControlAdapter(context)
 
